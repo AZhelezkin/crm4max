9ee88c3b32420e209dcd35ad04246e64539a4c2d
Blackmoo <aszhelezkin@gmail.com>
2026-04-05 02:38:00 +0300

    chore: remove client-app from repo

--- client-app/src/api/auth.api.ts
+++
@@ -1,9 +0,0 @@
-import { api } from './client'
-
-export const authApi = {
-  loginWithMax: (params: { init_data: string }) =>
-    api.post<{ token: string; userId: string; role: string }>('/auth/max', {
-      ...params,
-      role: 'client',
-    }).then((r) => r.data),
-}
